a54dc1529827a029d76460c082fb0ea7c488b6a5
Kadu Simoes <kadu_simoes_1@hotmail.com>
2018-02-10 01:54:43 -0200

    Facil desafio 2 OK

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -6,4 +6,9 @@ class CampaignMailer < ApplicationMailer
     @friend = friend
     mail to: @member.email, subject: "Nosso Amigo Secreto: #{@campaign.title}"
   end
+
+  def raffle_error(campaign)
+    @campaign = campaign
+    mail to: @campaign.user.email, subject: "Erro no sorteio do Nosso Amigo Secreto: #{@campaign.title}"
+  end
 end
--- spec/mailers/campaign_mailer_spec.rb
+++ spec/mailers/campaign_mailer_spec.rb
@@ -1,14 +1,15 @@
 require "rails_helper"
 
 RSpec.describe CampaignMailer, type: :mailer do
+
   describe "raffle" do
 
     before do
       @campaign = create(:campaign)
       @member   = create(:member, campaign: @campaign)
+      @friend   = create(:member, campaign: @campaign)
       @mail = CampaignMailer.raffle(@campaign, @member, @friend)
     end
-
 
     it "renders the headers" do
       expect(@mail.subject).to eq("Nosso Amigo Secreto: #{@campaign.title}")
@@ -19,6 +20,10 @@ RSpec.describe CampaignMailer, type: :mailer do
       expect(@mail.body.encoded).to match(@member.name)
     end
 
+    it "body have frind name" do
+      expect(@mail.body.encoded).to match(@friend.name)
+    end
+
     it "body have campaign creator name" do
       expect(@mail.body.encoded).to match(@campaign.user.name)
     end
@@ -26,6 +31,25 @@ RSpec.describe CampaignMailer, type: :mailer do
     it "body have member link to set open" do
       expect(@mail.body.encoded).to match("/members/#{@member.token}/opened")
     end
+
+  end
+
+  describe "raffle error" do
+
+    before do
+      @campaign = create(:campaign)
+      @mail = CampaignMailer.raffle_error(@campaign)
+    end
+
+    it "renders the headers" do
+      expect(@mail.subject).to eq("Erro no sorteio do Nosso Amigo Secreto: #{@campaign.title}")
+      expect(@mail.to).to eq([@campaign.user.email])
+    end
+
+    it "body have campaign creator name" do
+      expect(@mail.body.encoded).to match(@campaign.user.name)
+    end
+
   end
 
 end
