e597956a0c15b67fe805282c4b64e6e840115c86
Kadu Simoes <kadu_simoes_1@hotmail.com>
2018-02-10 20:30:53 -0200

    Medio desafio 1 OK

--- spec/jobs/campaign_raffle_job_spec.rb
+++ spec/jobs/campaign_raffle_job_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe CampaignRaffleJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include ActiveJob::TestHelper
+
+  let(:campaign) { create(:campaign) }
+  let(:member) { create(:member, campaign: @campaign) }
+  let(:friend) { create(:member, campaign: @campaign) }
+  let(:mail) { CampaignMailer.raffle(@campaign, @member, @friend) }
+
+  # OR
+  # before do
+  #   @campaign = create(:campaign)
+  #   @member   = create(:member, campaign: @campaign)
+  #   @friend   = create(:member, campaign: @campaign)
+  #   @mail = CampaignMailer.raffle(@campaign, @member, @friend)
+  # end
+
+  describe "#email job" do
+
+    it "when send email to members" do
+      ActiveJob::Base.queue_adapter = :test
+      expect {
+        CampaignRaffleJob.perform_later('raffle')
+      }.to have_enqueued_job
+    end
+
+  end
+
 end
